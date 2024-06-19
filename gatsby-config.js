bab4383a7c38af530ba910c9b9a11cbf43aeda17
byanko55 <byanko55@gmail.com>
2024-06-19 17:10:07 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `MacOS-like theme`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
